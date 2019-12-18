37a5dcd128cca220ecbeb3fa2b4ca62ce3d638f0
Jon Jander <jon.jander@lanmat.se>
2019-12-18 15:49:51 +0100

    update 1

--- SmartToy.ts
+++ SmartToy.ts
@@ -1,44 +1,55 @@
-pins.A0.onEvent(PinEvent.Fall, function() {
+pins.A0.onEvent(PinEvent.Fall, function () {
     // Control pins
     light.showAnimation(light.rainbowAnimation, 2000)
 })
-
 function SetSwitchLED(led: number, state: boolean) {
-    register = 0
     if (state == true) {
-        writeByteBuffered = writeByteBuffered | bit(led);
+        ledState = ledState | bit(led);
     } else {
-        writeByteBuffered = writeByteBuffered & ~bit(led);
+        ledState = ledState & ~bit(led);
     }
+    ledState &= writeMode //only set write pins
     pins.i2cWriteNumber(
-        33,
-        0,
+        ledAddress,
+        ledState,
         NumberFormat.Int8LE,
         false
     )
 }
-let register = 0
-let valueResult = 0
-let ca = 0
-let writeByteBuffered = 0
 
+function ReadButtons() {
+    let read = pins.i2cReadNumber(
+        inputAddress,
+        NumberFormat.Int8LE,
+        false
+    )
+    buttonStates = read & readMode
+}
+
+let writeMode = 0b00000001 //0b00000001
+let readMode = 0b11111111 //0b11111111
+let inputAddress = 0x20 //0x20
+let ledAddress = 0x21 //0x21
+let register = 0
+let buttonStates = 0
+let ledState = 0
 function bit(pin: number): number {
     return 1 << pin
 }
-forever(function() {
-    valueResult = pins.i2cReadNumber(
+forever(function () {
+    let valueResult = pins.i2cReadNumber(
         32,
         NumberFormat.Int8LE,
         false
     )
-    ca = 1
-    for (let index2 = 0; index2 <= 7; index2++) {
-        let register2 = valueResult & ca
-        if (register2 != 0) {
-            light.setPixelColor(index2, 0xff0000)
-        } else {
-            light.setPixelColor(index2, 0x00ff00)
+    for (let index = 0; index <= 7; index++) {
+        let reg = valueResult & bit(index)
+        if ((reg && readMode) > 0) {
+            if (reg != 0) {
+                light.setPixelColor(index, 0xff0000)
+            } else {
+                light.setPixelColor(index, 0x00ff00)
+            }
         }
-        ca = ca * 2
     }
 })
