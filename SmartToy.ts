31338e939cc55ca19386a44535ff2a8734537387
Jon Jander <jon.jander@lanmat.se>
2020-03-15 18:47:01 +0100

    fixed led switch mapping

--- SmartToy.ts
+++ SmartToy.ts
@@ -7,6 +7,27 @@ function SetSwitchLED(led: number, state: boolean) {
     ledState &= writeMode
     SendLedState()
 }
+
+function GetSwitchLed(led: number): number {
+    switch (led) {
+        case powerBtn:
+            return 0;
+        case redBtn:
+            return redBtnLed;
+        case blueBtn:
+            return blueBtnLed;
+        case engineBtn:
+            return engineBtnLed;
+        case greenBtn:
+            return greenBtnLed;
+        default:
+            return led;
+        
+    }
+
+}
+
+
 function SendLedState() {
     pins.i2cWriteNumber(
         ledAddress,
@@ -52,6 +73,24 @@ let read = 0
 let valueResult = 0
 let writeMode = 255
 let readMode = 255
+
+let powerBtn       = 0b1
+let redBtn         = 0b10
+let blueBtn        = 0b100
+let yellowBtn      = 0b1000
+let engineBtn      = 0b10000
+let greenBtn       = 0b100000
+let blueSwitch     = 0b1000000
+let redSwitch      = 0b10000000
+  
+let engineBtnLed   = 0b1
+let greenBtnLed    = 0b100
+let yellowBtnLed   = 0b1000
+let redBtnLed      = 0b10000
+let blueBtnLed     = 0b100000
+let blueSwitchLed  = 0b1000000
+let redSwitchLed   = 0b10000000
+
 inputAddress = 32
 ledAddress = 33
 ResetInputs()
@@ -61,8 +100,6 @@ function bit(pin: number): number {
 }
 SetSwitchLED(0, true)
 forever(function () {
-
-
     valueResult = pins.i2cReadNumber(
         32,
         NumberFormat.Int8LE,
@@ -73,10 +110,10 @@ forever(function () {
         if ((bit(index) && readMode) > 0) {
             if (reg != 0) {
                 light.setPixelColor(index, 0xff0000)
-                SetSwitchLED(index, false)
+                SetSwitchLED(GetSwitchLed(index), false)
             } else {
                 light.setPixelColor(index, 0x00ff00)
-                SetSwitchLED(index, true)
+                SetSwitchLED(GetSwitchLed(index), true)
             }
         }
     }
